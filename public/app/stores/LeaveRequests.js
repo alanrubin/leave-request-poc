df88e2dc41637607450fcd1712bef4d44d691f91
Alan Rubin <alan@frubin.com>
2011-09-28 12:12:29 +0300

    Using json store for data still with local browser storage for write

--- public/app/stores/LeaveRequests.js
+++ public/app/stores/LeaveRequests.js
@@ -1,4 +1,12 @@
 App.stores.leaveRequests = new Ext.data.Store({
     model: 'LeaveRequest',
+	proxy: {
+	            type: 'ajax',
+	            url : 'leaverequests.json',
+	            reader: {
+	                type: 'json',
+	                root: 'leaverequests'
+	            }
+	        },
     autoLoad: true
 });
